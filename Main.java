fc18b3374bd2d4cc43d55a00fdf6c22487e847a0
Pavel <Technic47@gmail.com>
2023-02-06 14:45:40 +0300

    Revert "Changes Del"
    This reverts commit 8115bfa377bc3a412674e5bac9589427141e0e04.

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Starting");
         System.out.println("Farting");
+        System.out.println("Retarding");
     }
 }
