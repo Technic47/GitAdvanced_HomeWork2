8115bfa377bc3a412674e5bac9589427141e0e04
Pavel <Technic47@gmail.com>
2023-02-06 14:44:53 +0300

    Changes Del

--- Main.java
+++ Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Starting");
         System.out.println("Farting");
-        System.out.println("Retarding");
     }
 }
